3a55f48ebed057c0c628f30becc3f0a37b991afc
Gio2025 <george.athanasopoulos46@student.qcc.cuny.edu>
2026-01-29 12:45:03 -0500

    Update Testing

--- Lab2/Lab2_Athanasopoulos.py
+++ Lab2/Lab2_Athanasopoulos.py
@@ -5,3 +5,7 @@ Lab 2: GIT commands
 
 username = input("Enter a username: ")
 print(f"\nWelcome to Git Commands {username}")
+
+# testing lines
+num = 8
+print(f"Double of number {num} is {num * 2}")
