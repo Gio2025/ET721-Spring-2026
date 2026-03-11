5aeb1da508c638c8cc4bf6f3dfeff5e51a67c319
Gio2025 <george.athanasopoulos46@student.qcc.cuny.edu>
2026-03-11 17:21:14 +0000

    Completed Lab 11

--- Lab11/app.py
+++ Lab11/app.py
@@ -6,28 +6,28 @@ Lab 11, Introduction to Flask
 
 from flask import Flask, render_template
 
-
 # create an object 'app' from the flask module
 app = Flask(__name__)
 
 # set the routing to the main page
-# 'route' decorator is used to access the root URL
 @app.route('/')
 def index():
     name = "George Athanasopoulos"
     fruits = ['apple', 'orange', 'grapes']
-    fruit = 'orange' 
+    fruit = 'orange'
     return render_template('index.html', username = name, listfruits = fruits, f = fruit )
 
-# endpoints refer to the name of the view in an app
+# about page
 @app.route('/about')
 def about():
-    return '<h1>About Us</h1>'
+    images = ['mountain.jpg', 'desert.jpg', 'ocean.jpg']
+    return render_template('about.html', listimages = images)
 
+# quotes page
 @app.route('/quotes')
 def quotes():
     return '<h1>Quotes</h1>'
 
-# set the 'app' to run if you execute the file directly (not when it is imported)
+# run the app
 if __name__ == '__main__':
     app.run(debug = True)
