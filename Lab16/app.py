41f21b03e669e53780d97ae74216f3d4b83974f5
Gio2025 <george.athanasopoulos46@student.qcc.cuny.edu>
2026-04-16 17:35:05 +0000

    Completed Lab 16

--- Lab16/app.py
+++ Lab16/app.py
@@ -55,7 +55,22 @@ def add_task():
     return jsonify({'status': 'error'}) 
 
 #------------------
+# Delete a task
+#------------------
+@app.route('/delete_task', methods = ['POST'])
+def delete_task():
+    data = request.get_json()
+    task_id = data.get('id')
+
+    cursor = db.cursor()
+    cursor.execute("DELETE FROM tasks WHERE id = %s", (task_id,))
+    db.commit()
+    cursor.close()
+    return jsonify({'status' : 'deleted'})
+
+#------------------
 # Run app
 #------------------
 if __name__ == '__main__':
     app.run(debug=True)
+
